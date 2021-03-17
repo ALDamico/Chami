6996369600d4b91fc312ac54bb0fee2355d2c1c3
Andrea Luciano Damico <aldamico90@gmail.com>
2021-03-18 00:34:37 +0100

    Added WhereAmI.csproj command line utility

---
+++ WhereAmI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhereAmI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentEnvironment = Environment.GetEnvironmentVariable("_CHAMI_ENV");
+            if (string.IsNullOrEmpty(currentEnvironment))
+            {
+                currentEnvironment = Environment.GetEnvironmentVariable("USER");
+            }
+
+            if (string.IsNullOrEmpty(currentEnvironment))
+            {
+                Console.WriteLine("Unable to find the current environment!");
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine(currentEnvironment);
+            Environment.Exit(0);
+        }
+    }
+}
