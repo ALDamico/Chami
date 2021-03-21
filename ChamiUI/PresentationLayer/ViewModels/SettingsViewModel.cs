705c19752e28740f1e41ef75618b4b61e0f6731c
Andrea Luciano Damico <aldamico90@gmail.com>
2021-03-21 19:57:45 +0100

    Binding to multiple controllers

--- ChamiUI/PresentationLayer/ViewModels/SettingsViewModel.cs
+++ ChamiUI/PresentationLayer/ViewModels/SettingsViewModel.cs
@@ -1,7 +1,40 @@
+using System.Collections.Generic;
+using System.Windows.Controls;
+using ChamiUI.Controls;
+
 namespace ChamiUI.PresentationLayer.ViewModels
 {
-    public class SettingsViewModel:ViewModelBase
+    public class SettingsViewModel : ViewModelBase
     {
-        
+        public SettingsViewModel()
+        {
+            _controls = new Dictionary<string, UserControl>();
+            _controls["View"] = new ConsoleAppearanceEditor();
+        }
+        private Dictionary<string, UserControl> _controls;
+
+        public void ChangeControl(string name)
+        {
+            var controlExists = _controls.TryGetValue(name, out var control);
+            if (controlExists)
+            {
+                DisplayedControl = control;
+            }
+            else
+            {
+                DisplayedControl = null;
+            }
+        }
+        private UserControl _displayedControl;
+
+        public UserControl DisplayedControl
+        {
+            get => _displayedControl;
+            set
+            {
+                _displayedControl = value;
+                OnPropertyChanged(nameof(DisplayedControl));
+            }
+        }
     }
 }
--- ChamiUI/Windows/SettingsWindow/SettingsWindow.xaml.cs
+++ ChamiUI/Windows/SettingsWindow/SettingsWindow.xaml.cs
@@ -1,12 +1,23 @@
 using System.Windows;
+using System.Windows.Controls;
+using ChamiUI.PresentationLayer.ViewModels;
 
 namespace ChamiUI.Windows.SettingsWindow
 {
     public partial class SettingsWindow : Window
     {
+        private readonly SettingsViewModel _settingsViewModel;
         public SettingsWindow()
         {
+            _settingsViewModel = new SettingsViewModel();
+            DataContext = _settingsViewModel;
             InitializeComponent();
+        }
+
+        private void CategoriesTree_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var name = (e.NewValue as TreeViewItem)?.Header.ToString();
+            _settingsViewModel.ChangeControl(name);
         }
     }
 }
