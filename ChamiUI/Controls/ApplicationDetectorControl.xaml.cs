0b05fcedf1e4489eea32baff9831d4c38dc928dc
Andrea Luciano Damico <aldamico90@gmail.com>
2021-04-05 16:17:44 +0200

    Added entity

---
+++ ChamiUI/Controls/ApplicationDetectorControl.xaml.cs
@@ -0,0 +1,18 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChamiUI.Controls
+{
+    public partial class ApplicationDetectorControl : UserControl
+    {
+        public ApplicationDetectorControl()
+        {
+            InitializeComponent();
+        }
+
+        private void AddApplicationButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- ChamiUI/PresentationLayer/ViewModels/SettingsWindowViewModel.cs
+++ ChamiUI/PresentationLayer/ViewModels/SettingsWindowViewModel.cs
@@ -17,6 +17,7 @@ namespace ChamiUI.PresentationLayer.ViewModels
             _controls["View"] = new ConsoleAppearanceEditor(Settings.ConsoleAppearanceSettings);
             _controls["Logging"] = new LoggingSettingsEditor(Settings.LoggingSettings);
             _controls["Safety"] = new SafeVariableEditor(Settings.SafeVariableSettings);
+            _controls["Detector"] = new ApplicationDetectorControl();
             DisplayedControl = _controls.Values.FirstOrDefault();
         }
 
