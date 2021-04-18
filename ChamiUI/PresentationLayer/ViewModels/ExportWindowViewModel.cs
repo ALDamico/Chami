f87d30032552604e595a4f2b0535937d21383cdd
Andrea Luciano Damico <aldamico90@gmail.com>
2021-04-18 03:03:01 +0200

    Initial version of ExportWindow

---
+++ ChamiUI/PresentationLayer/ViewModels/ExportWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamiUI.PresentationLayer.ViewModels
+{
+    public class ExportWindowViewModel:ViewModelBase
+    {
+        public ExportWindowViewModel()
+        {
+            Environments = new ObservableCollection<EnvironmentViewModel>();
+        }
+        public ExportWindowViewModel(ICollection<EnvironmentViewModel> environments) : this()
+        {
+            foreach (var environment in environments)
+            {
+                Environments.Add(environment);
+            }
+        }
+        public ObservableCollection<EnvironmentViewModel> Environments { get; set; }
+        public bool ExportSelected { get => _exportSelected;
+            set
+            {
+                _exportSelected = value;
+                OnPropertyChanged(nameof(ExportSelected));
+            }
+        }
+        public bool ExportAll
+        {
+            get => _exportAll;
+            set
+            {
+                _exportAll = value;
+                OnPropertyChanged(nameof(ExportAll));
+            }
+        }
+
+        private bool _exportSelected;
+        private bool _exportAll;
+    }
+}
---
+++ ChamiUI/Windows/ExportWindow/ExportWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ChamiUI.PresentationLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChamiUI.Windows.ExportWindow
+{
+    /// <summary>
+    /// Logica di interazione per ExportWindow.xaml
+    /// </summary>
+    public partial class ExportWindow : Window
+    {
+        public ExportWindow(ICollection<EnvironmentViewModel> environments)
+        {
+            _viewModel = new ExportWindowViewModel(environments);
+            
+            DataContext = _viewModel;
+            InitializeComponent();
+        }
+
+        private ExportWindowViewModel _viewModel;
+    }
+}
