ba0b07ec9fb49191ff6ea58eb3021bd77ef2ef8d
Andrea Luciano Damico <aldamico90@gmail.com>
2021-05-16 02:28:21 +0200

    Refactoring
    Refactored EnvironmentVariableApplicationCommand.cs to eliminate duplicated code.

--- ChamiUI/BusinessLayer/EnvironmentVariableApplicationCommand.cs
+++ ChamiUI/BusinessLayer/EnvironmentVariableApplicationCommand.cs
@@ -18,30 +18,35 @@ namespace ChamiUI.BusinessLayer
         public void Execute()
         {
             var arguments = $"/C SETX {EnvironmentVariable.Name} {EnvironmentVariable.Value}";
-            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", arguments);
-            processStartInfo.RedirectStandardError = true;
-            processStartInfo.RedirectStandardOutput = true;
-            Process process = new Process();
-            process.StartInfo = processStartInfo;
+            var process = PrepareProcess(arguments);
             process.Start();
+        }
+
+        private Process PrepareProcess(string arguments)
+        {   
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", arguments)
+            {
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            Process process = new Process()
+            {
+                StartInfo = processStartInfo
+            };
+            return process;
         }
 
         public async Task ExecuteAsync(IProgress<CmdExecutorProgress> progress)
         {
             var arguments = $"/C SETX \"{EnvironmentVariable.Name}\" \"{EnvironmentVariable.Value}\"";
             var commandLineFull = "cmd.exe " + arguments;
-            progress?.Report(new CmdExecutorProgress(0, null, commandLineFull));
-            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", arguments);
-            processStartInfo.RedirectStandardError = true;
-            processStartInfo.RedirectStandardOutput = true;
-            processStartInfo.CreateNoWindow = true;
-            Process process = new Process();
-            process.StartInfo = processStartInfo;
+            var process = PrepareProcess(arguments);
             process.Start();
             await process.WaitForExitAsync();
             if (progress != null)
             {
-                progress.Report(new CmdExecutorProgress(0, process.StandardOutput.BaseStream, null));
+                progress.Report(new CmdExecutorProgress(0, process.StandardOutput.BaseStream, commandLineFull));
             }
         }
     }
