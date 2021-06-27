09f742339d7e26d20d56ad7ea456650535f53bce
Andrea Luciano Damico <aldamico90@gmail.com>
2021-06-27 23:01:03 +0200

    Added documentation Added documentation to IMinimizationStrategy.cs and its subclasses.

--- ChamiUI/PresentationLayer/Minimizing/MinimizeToTrayStrategy.cs
+++ ChamiUI/PresentationLayer/Minimizing/MinimizeToTrayStrategy.cs
@@ -4,21 +4,39 @@ using ChamiUI.Localization;
 
 namespace ChamiUI.PresentationLayer.Minimizing
 {
-    public class MinimizeToTrayStrategy: IMinimizationStrategy
+    /// <summary>
+    /// Represents the act of minimizing the window to the system tray.
+    /// </summary>
+    public class MinimizeToTrayStrategy : IMinimizationStrategy
     {
+        /// <summary>
+        /// Perform the action of minimizing the window by hiding it.
+        /// </summary>
+        /// <param name="window">The <see cref="Window"/> object to apply the minimizationn to.</param>
+        /// <param name="action">Additional code to execute when minimizing.</param>
         public void Minimize(Window window, Action action)
         {
             if (action != null)
             {
                 action.Invoke();
             }
-            
+
             window.Hide();
         }
 
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
         private static IMinimizationStrategy _instance;
 
+        /// <summary>
+        /// Localizable name
+        /// </summary>
         public string Name => ChamiUIStrings.MinimizeToTrayStrategyName;
+
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
         public static IMinimizationStrategy Instance
         {
             get
