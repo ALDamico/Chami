80d45a91c01822ee099c1d8b1fc4c1325ae1fe52
Andrea Luciano Damico <luciano.damico@rbtechnology.it>
2021-07-02 14:58:25 +0200

    Documentation of CmdExecutor.cs

--- ChamiUI/BusinessLayer/CmdExecutor.cs
+++ ChamiUI/BusinessLayer/CmdExecutor.cs
@@ -17,18 +17,32 @@ using Environment = Chami.Db.Entities.Environment;
 
 namespace ChamiUI.BusinessLayer
 {
+    /// <summary>
+    /// Executes sequences of Windows shells commands.
+    /// </summary>
     public class CmdExecutor
     {
+        /// <summary>
+        /// Constructs a new CmdExecutor object and initializes the command queue.
+        /// </summary>
         public CmdExecutor()
         {
             EnvironmentVariablesToApply = new List<IEnvironmentVariableCommand>();
         }
 
+        /// <summary>
+        /// Constructs a new <see cref="CmdExecutor"/> object and sets the <see cref="TargetEnvironment"/> property.
+        /// </summary>
+        /// <param name="targetEnvironment">The environment that is being targeted.</param>
         public CmdExecutor(Environment targetEnvironment) : this()
         {
             TargetEnvironment = targetEnvironment;
         }
 
+        /// <summary>
+        /// Constructs a new <see cref="CmdExecutor"/> object and sets the <see cref="TargetEnvironment"/> property.
+        /// </summary>
+        /// <param name="targetEnvironmentViewModel">The environment that is being targeted.</param>
         public CmdExecutor(EnvironmentViewModel targetEnvironmentViewModel) : this()
         {
             var converter = new EnvironmentConverter();
@@ -36,22 +50,43 @@ namespace ChamiUI.BusinessLayer
             TargetEnvironment = convertedEnvironment;
         }
         
+        /// <summary>
+        /// The environment to target.
+        /// </summary>
         public Environment TargetEnvironment { get; }
 
+        /// <summary>
+        /// Adds a new <see cref="IEnvironmentVariableCommand"/> to the command queue.
+        /// </summary>
+        /// <param name="command">The command to perform when the <see cref="Execute"/> or <see cref="ExecuteAsync"/> methods are invoked.</param>
         public void AddCommand(IEnvironmentVariableCommand command)
         {
             EnvironmentVariablesToApply.Add(command);
         }
 
+        /// <summary>
+        /// An event that gets fired when an the target environment changes.
+        /// </summary>
         public event EventHandler<EnvironmentChangedEventArgs> EnvironmentChanged;
 
+        /// <summary>
+        /// Event handler for the <see cref="EnvironmentChanged"/> event.
+        /// </summary>
+        /// <param name="sender">The object that sends the request. Unused.</param>
+        /// <param name="args">Information about the environment that has changed.</param>
         protected virtual void OnEnvironmentChanged(object sender, EnvironmentChangedEventArgs args)
         {
             EnvironmentChanged?.Invoke(this, args);
         }
 
+        /// <summary>
+        /// The queue of commands to execute.
+        /// </summary>
         protected List<IEnvironmentVariableCommand> EnvironmentVariablesToApply { get; }
 
+        /// <summary>
+        /// Performs all the commands in the <see cref="EnvironmentVariablesToApply"/> queue synchronously.
+        /// </summary>
         public void Execute()
         {
             foreach (var environmentVariable in EnvironmentVariablesToApply)
@@ -60,6 +95,12 @@ namespace ChamiUI.BusinessLayer
             }
         }
 
+        /// <summary>
+        /// Performs all the commands in the <see cref="EnvironmentVariablesToApply"/> queue asynchronously.
+        /// It optionally notifies of progress and execution can be canceled.
+        /// </summary>
+        /// <param name="progress">Notifies of progress.</param>
+        /// <param name="cancellationToken">Allows canceling the execution midway and revert to a previous state.</param>
         public async Task ExecuteAsync(IProgress<CmdExecutorProgress> progress, CancellationToken cancellationToken)
         {
             var message = ChamiUIStrings.StartingExecutionMessage;
--- ChamiUI/BusinessLayer/Validators/ValidationUtils.cs
+++ ChamiUI/BusinessLayer/Validators/ValidationUtils.cs
@@ -3,8 +3,17 @@ using ChamiUI.PresentationLayer.ViewModels;
 
 namespace ChamiUI.BusinessLayer.Validators
 {
+    /// <summary>
+    /// Helper class for functions related to validation.
+    /// </summary>
     public static class ValidationUtils
     {
+        /// <summary>
+        /// Converts an <see cref="object"/> to an <see cref="EnvironmentVariableViewModel"/> that the validation rules can use.
+        /// The function attempts to convert from <see cref="BindingGroup"/> and from <see cref="EnvironmentVariableViewModel"/>.
+        /// </summary>
+        /// <param name="value">The object to convert from.</param>
+        /// <returns></returns>
         public static EnvironmentVariableViewModel ConvertObjectToValidate(object value)
         {
             if (value is BindingGroup bindingGroup)
