bbcc66e0ccd54c83ec061730a6c591b1d02568fc
Andrea Luciano Damico <aldamico90@gmail.com>
2021-07-04 02:02:55 +0200

    Fixed issues with tests and with exporter
    InsertTest now deletes the previously inserted Environment if it already exists.
    Temporary file cleanup for ExportTests.cs
    Excel Application Quit method called inside try-catch block.

--- ChamiTests/ExportTests.cs
+++ ChamiTests/ExportTests.cs
@@ -1,30 +1,64 @@
 using System;
+using System.IO;
 using Chami.Db.Entities;
 using ChamiUI.BusinessLayer.Exporters;
+using Microsoft.VisualBasic.FileIO;
 using Xunit;
 using Environment = Chami.Db.Entities.Environment;
 
 namespace ChamiTests
 {
-    public class ExportTests
+    public class ExportTests : IDisposable
     {
+        private readonly string _asyncFileLocation = SpecialDirectories.MyDocuments + "/test_async.xlsx";
+        private readonly string _fileLocation = SpecialDirectories.MyDocuments + "/test.xlsx";
         [Fact]
         public void TestExcelExport()
         {
             using (var exporter = new EnvironmentExcelExporter())
             {
-                var environment1 = new Environment() {Name = "Test", AddedOn = DateTime.Now, EnvironmentType = 0};
-                environment1.EnvironmentVariables.Add(new EnvironmentVariable(){Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
-                environment1.EnvironmentVariables.Add(new EnvironmentVariable(){Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
-                environment1.EnvironmentVariables.Add(new EnvironmentVariable(){Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
-                var environment2 = new Environment(){Name = "Test2", AddedOn = DateTime.Now, EnvironmentType = 0};
-                environment2.EnvironmentVariables.Add(new EnvironmentVariable(){Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
-                environment2.EnvironmentVariables.Add(new EnvironmentVariable(){Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
-                environment2.EnvironmentVariables.Add(new EnvironmentVariable(){Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
-                exporter.AddEnvironment(environment1);
-                exporter.AddEnvironment(environment2);
-                exporter.Export("d:\\test.xlsx");
+                ConfigureExporter(exporter);
+                exporter.Export("test.xlsx");
+                Assert.True(File.Exists(_fileLocation));
             }
+        }
+
+        [Fact]
+        public async void TestExcelExportAsync()
+        {
+            using (var exporter = new EnvironmentExcelExporter())
+            {
+                ConfigureExporter(exporter);
+                await exporter.ExportAsync("test_async.xlsx");
+                Assert.True(File.Exists(_asyncFileLocation));
+            }
+        }
+
+        private static void ConfigureExporter(EnvironmentExcelExporter exporter)
+        {
+            var environment1 = new Environment() {Name = "Test", AddedOn = DateTime.Now, EnvironmentType = 0};
+            environment1.EnvironmentVariables.Add(new EnvironmentVariable()
+                {Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
+            environment1.EnvironmentVariables.Add(new EnvironmentVariable()
+                {Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
+            environment1.EnvironmentVariables.Add(new EnvironmentVariable()
+                {Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
+            var environment2 = new Environment() {Name = "Test2", AddedOn = DateTime.Now, EnvironmentType = 0};
+            environment2.EnvironmentVariables.Add(new EnvironmentVariable()
+                {Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
+            environment2.EnvironmentVariables.Add(new EnvironmentVariable()
+                {Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
+            environment2.EnvironmentVariables.Add(new EnvironmentVariable()
+                {Name = "USER", Value = "MyValue", AddedOn = DateTime.Now, Environment = environment1});
+            exporter.AddEnvironment(environment1);
+            exporter.AddEnvironment(environment2);
+        }
+
+        public void Dispose()
+        {
+            File.Delete(_asyncFileLocation);
+            File.Delete(_fileLocation);
+            GC.SuppressFinalize(this);
         }
     }
 }
