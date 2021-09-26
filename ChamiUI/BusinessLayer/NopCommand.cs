796df3f66b2295bf4268b1d44e39b9b7f4d40fb4
Andrea Luciano Damico <aldamico90@gmail.com>
2021-09-26 18:38:54 +0200

    Added documentation
    Documentation of NopCommand.cs

--- ChamiUI/BusinessLayer/NopCommand.cs
+++ ChamiUI/BusinessLayer/NopCommand.cs
@@ -7,26 +7,49 @@ using Chami.Db.Entities;
 
 namespace ChamiUI.BusinessLayer
 {
+    /// <summary>
+    /// Represent a no-op command for use by <see cref="CmdExecutorBase"/> and its derived classes
+    /// </summary>
     public class NopCommand : IShellCommand
     {
+        /// <summary>
+        /// Constructs a new <see cref="NopCommand"/> object and sets its custom message.
+        /// </summary>
+        /// <param name="customMessage"></param>
         public NopCommand(string customMessage)
         {
             _customMessage = customMessage;
         }  
+        
+        /// <summary>
+        /// NO-OP
+        /// </summary>
         public void Execute()
         {
             
         }
 
+        /// <summary>
+        /// Reports the custom message.
+        /// </summary>
+        /// <param name="percentage">The percentage completion.</param>
+        /// <param name="cancellationToken">Unused. Can be null or <see cref="CancellationToken.None"/>.</param>
         public async Task ExecuteAsync(float percentage, CancellationToken cancellationToken)
         {
             Progress.Report(new CmdExecutorProgress(percentage, _customMessage));
         }
 
+        /// <summary>
+        /// Handles progress reporting.
+        /// </summary>
         public IProgress<CmdExecutorProgress> Progress { get; set; }
 
         private string _customMessage;
 
+        /// <summary>
+        /// Sets a new custom message.
+        /// </summary>
+        /// <param name="customMessage">The new custom message.</param>
         public void SetCustomMessage(string customMessage)
         {
             _customMessage = customMessage;
