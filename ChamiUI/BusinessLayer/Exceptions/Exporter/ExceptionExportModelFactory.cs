edd24accfab91773fb0b08ca9d6f0193d6d0a9ec
Andrea Luciano Damico <aldamico90@gmail.com>
2022-10-09 15:23:13 +0200

    Code cleanup

--- ChamiUI/BusinessLayer/Exceptions/Exporter/ExceptionExportModelFactory.cs
+++ ChamiUI/BusinessLayer/Exceptions/Exporter/ExceptionExportModelFactory.cs
@@ -8,21 +8,21 @@ public static class ExceptionExportModelFactory
 {
     public static ExceptionExportModel GetExceptionExportModel(Exception exception, string logPath)
     {
-        ExceptionExportModel exceptionExportModel = new();
-
-        exceptionExportModel.ExceptionMessage = exception.Message;
-        exceptionExportModel.ExceptionName = exception.GetType().FullName;
-        exceptionExportModel.ExceptionSource = exception.Source;
-        exceptionExportModel.StackTrace = exception.StackTrace;
-        exceptionExportModel.GenerationDate = DateTime.Now;
-        exceptionExportModel.OperatingSystem = Environment.OSVersion.ToString();
+        ExceptionExportModel exceptionExportModel = new()
+        {
+            ExceptionMessage = exception.Message,
+            ExceptionName = exception.GetType().FullName,
+            ExceptionSource = exception.Source,
+            StackTrace = exception.StackTrace,
+            GenerationDate = DateTime.Now,
+            OperatingSystem = Environment.OSVersion.ToString()
+        };
 
         using var logFile = File.Open(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         var bytes = new byte[logFile.Length]; 
         logFile.Read(bytes, 0, (int)logFile.Length);
-
         
-        exceptionExportModel.Log = System.Text.Encoding.UTF8.GetString(bytes);;
+        exceptionExportModel.Log = System.Text.Encoding.UTF8.GetString(bytes);
         
         return exceptionExportModel;
     }
